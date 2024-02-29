b5b44a6a3649a57b23130f411914dbd251188932
xianqing Jia <149289310+xianqingjia777@users.noreply.github.com>
2024-02-29 23:08:22 +0800

    Update run_comparison_algorithm.py
    update

--- run_comparison_algorithm.py
+++ run_comparison_algorithm.py
@@ -1,12 +1,28 @@
+
 from comparison_algorithm import comparison_algorithm
 import time
 import logging
 import os
 import pandas as pd
 
-# agent = "auto-ui"
-agent = "appagent"
-log_dir_path = f"/data/jxq/mobile-agent/comparison_algorithm/log/{agent}"
+dir_map = dict()
+
+
+part = ""
+classes = "web_shopping"
+# classes = "general"
+# classes = "install"
+# classes = "googleapps"
+
+agent = "auto-ui"
+COSSINE_BOUND = 0.7
+# agent = "appagent"
+log_dir_path = f"/data/jxq/mobile-agent/comparison_algorithm/log/{agent}/{classes}"
+
+dir_map["web_shopping"] = f"/data/zzh/mobile-agent/Auto-UI/agentenv/agent_result/{classes}/"
+dir_map["general"] = f"/data/zzh/mobile-agent/Auto-UI/agentenv/agent_result/{classes}/"
+dir_map["install"] = f"/data/zzh/mobile-agent/Auto-UI/agentenv/agent_result/{classes}/"
+dir_map["googleapps"] = f"/data/zzh/mobile-agent/Auto-UI/agentenv/agent_result/google_apps/"
 
 # 获取当前时间
 current_time = time.localtime()
@@ -15,18 +31,14 @@ formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", current_time)
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
-                    handlers=[logging.FileHandler(f'{log_dir_path}/comparison_algorithm_{formatted_time}.log', 'w'),
+                    handlers=[logging.FileHandler(f'{log_dir_path}/COSSINE_BOUND_{COSSINE_BOUND}_comparison_algorithm_{formatted_time}.log', 'w'),
                             logging.StreamHandler()])
 
-# instruction_fp = "/data/jxq/all_instruction.csv"
-instruction_fp = "/data/jxq/all_instruction_appagent.csv"
-top_checkpoint_dir = f"/data/jxq/mobile-agent/aitw_replay_data/general/"
-# top_capture_dir = "/data/jxq/mobile-agent/agent-captured_data/auto-ui"
+instruction_fp = f"/data/jxq/mobile-agent/comparison_algorithm/tasks/{classes}/all_instruction{part}.csv"
+# instruction_fp = "/data/jxq/all_instruction_appagent-2.csv"
+top_checkpoint_dir = f"/data/jxq/mobile-agent/aitw_replay_data/{classes}/"
+top_capture_dir = dir_map[classes]
 
-#agent_app 
-top_capture_dir = "/data/wangshihe/AgentTestbed/AppAgent/tasks-240126-1/"
-second_capture_dir_ls = [os.path.join(top_capture_dir,dir) for dir in os.listdir(os.path.join(top_capture_dir)) if os.path.isdir(os.path.join(top_capture_dir,dir))]
-captured_epsoide_dir = [os.path.join(second_capture_dir,dir) for second_capture_dir in second_capture_dir_ls for dir in os.listdir(second_capture_dir) if os.path.isdir(os.path.join(second_capture_dir,dir))]
 
 
 checkpoint_dir_ls = []
@@ -40,12 +52,12 @@ output_data = pd.DataFrame(columns=["episode_id", "instruction", "success_flag"]
 for index, row in instruction_data.iterrows():
     trace_temp = row["trace_folder_path"].split("/")[-1]
     checkpoint_dir_ls.append(os.path.join(top_checkpoint_dir, trace_temp))
-    # captured_dir_ls.append(os.path.join(top_capture_dir, str(row["episode_id"])))
+    captured_dir_ls.append(os.path.join(top_capture_dir, str(row["episode_id"])))
     
-    for i in range(len(captured_epsoide_dir)):
-        if str(row["episode_id"]) in captured_epsoide_dir[i]:
-            captured_dir_ls.append(captured_epsoide_dir[i])
-            break
+    # for i in range(len(captured_epsoide_dir)):
+    #     if str(row["episode_id"]) in captured_epsoide_dir[i]:
+    #         captured_dir_ls.append(captured_epsoide_dir[i])
+    #         break
 
 
 
@@ -57,7 +69,7 @@ for i, row in instruction_data.iterrows():
     logging.info(f"checkpoint_dir: {checkpoint_dir}")
     logging.info(f"captured_dir: {captured_dir}")
 
-    if comparison_algorithm(checkpoint_dir, captured_dir):
+    if comparison_algorithm(checkpoint_dir, captured_dir, COSSINE_BOUND):
         logging.info(f"comparison_algorithm successfully: {checkpoint_dir} and {captured_dir}")
         testbed_judge_success_state.append(True)
     else:
@@ -67,12 +79,15 @@ for i, row in instruction_data.iterrows():
 for index, row in instruction_data.iterrows():
     output_data.loc[index] = [str(row["episode_id"]), row["instruction"], testbed_judge_success_state[index]]
 
-output_data.to_csv(f"/data/jxq/mobile-agent/comparison_algorithm/output_data/general/{agent}/{formatted_time}.csv", index=False)
+output_data.to_csv(f"/data/jxq/mobile-agent/comparison_algorithm/output_data/{classes}/{agent}/{formatted_time}_{part}_COSSINE_BOUND_{COSSINE_BOUND}.csv", index=False)
 logging.info(f"success_num: {testbed_judge_success_state.count(True)}")
 logging.info(f"total_instruction_num: {total_instruction_num}")
 logging.info(f"testbed judge success_rate: {testbed_judge_success_state.count(True)/total_instruction_num}")
 
-# human_judegement = pd.read_csv("/data/jxq/mobile-agent/comparison_algorithm/output_data/general/auto-ui-task-human.csv")
+
+# testbed_judge_success_state = pd.read_csv("/data/jxq/mobile-agent/comparison_algorithm/output_data/{classes}/appagent/2024-01-31 20:24:31.csv")
+# testbed_judge_success_state = list(testbed_judge_success_state["success_flag"])
+# human_judegement = pd.read_csv("/data/jxq/mobile-agent/comparison_algorithm/output_data/{classes}/appagent-task-human.csv")
 # human_judegement_state = list(human_judegement["success_flag"])
 
 # testbed_accuracy_state = [ts == hs for ts, hs in zip(testbed_judge_success_state,human_judegement_state)]
@@ -112,6 +127,7 @@ logging.info(f"testbed judge success_rate: {testbed_judge_success_state.count(Tr
 
 
 
+    
     
 
 
